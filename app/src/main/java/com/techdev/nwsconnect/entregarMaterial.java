e7d146c3dc39721ade9b44ccf5f669ad37dbafb4
Pablo Yuri Scherer de Souza <pabloyuriss@hotmail.com>
2020-08-13 00:44:57 -0300

    feat: spinner funcionando

--- app/src/main/java/com/techdev/nwsconnect/entregarMaterial.java
+++ app/src/main/java/com/techdev/nwsconnect/entregarMaterial.java
@@ -2,14 +2,21 @@ package com.techdev.nwsconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class entregarMaterial extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class entregarMaterial extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +24,70 @@ public class entregarMaterial extends AppCompatActivity implements AdapterView.O
         setContentView(R.layout.entregarmaterial);
 
         Spinner spinnermaterial = findViewById(R.id.spinner);
-        Spinner spinnerquantidade = findViewById(R.id.spinner2);
-        Spinner spinneratleta = findViewById(R.id.spinner3);
-
-        spinners(spinnermaterial);
-        spinners(spinnerquantidade);
-        spinners(spinneratleta);
 
 
+        // Initializing a String Array
+        String[] material = new String[]{
+                "Material:",
+                "Chuteira",
+                "Luva"
+        };
 
-    }
+        final List<String> plantsList = new ArrayList<>(Arrays.asList(material));
 
-    public void spinners (Spinner spinner) {
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Material, R.layout.item_spinner);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.item_spinner,plantsList){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.item_spinner);
+        spinnermaterial.setAdapter(spinnerArrayAdapter);
 
-    }
+        spinnermaterial.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+            }
+        });
     }
 }
+
