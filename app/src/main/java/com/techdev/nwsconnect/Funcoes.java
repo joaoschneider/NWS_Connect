1a04d556d60a22c1fb664dac8cccb82d82451c3a
joaoschneider <joaoschneider1007@live.com>
2020-08-14 13:55:27 -0300

    feat: busca de atletas no banco de dados ao iniciar o aplicativo (MainActivity.onCreate).
    Uma ArrayList<String> é criada a partir dos dados buscados e salva na variavel mAtletas, que pertence a Funcoes. Essa variavel pode ser acessada atraves das activities que extendem Funcoes.

--- app/src/main/java/com/techdev/nwsconnect/Funcoes.java
+++ app/src/main/java/com/techdev/nwsconnect/Funcoes.java
@@ -1,18 +1,29 @@
 package com.techdev.nwsconnect;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Funcoes extends AppCompatActivity {
+    static List<String> mAtletas;
+    private final String DEBUG_TAG = "NWSConnect";
 
     // Initializing a String Array
     String[] material = new String[]{
@@ -27,7 +38,7 @@ public class Funcoes extends AppCompatActivity {
                 "Society"
     };
 
-    public void setSpinner (Spinner spinnermaterial, String[] stringarray){
+    protected void setSpinner (Spinner spinnermaterial, String[] stringarray){
         final List<String> materialList = new ArrayList<>(Arrays.asList(stringarray));
 
         // Initializing an ArrayAdapter
@@ -50,15 +61,41 @@ public class Funcoes extends AppCompatActivity {
         });
     }
 
-    public void vaientregamaterial (View view){
+    protected void vaientregamaterial (View view){
         Intent entregamaterial = new Intent(this, EntregarMaterial.class);
         startActivity(entregamaterial);
 
     }
 
-    public void vainovomaterial (View view){
+    protected void vainovomaterial (View view){
         Intent novomaterial = new Intent(this, NovoMaterial.class);
         startActivity(novomaterial);
-
     }
+
+    protected void buscarAtletas(){
+        final List<String> atletas = new ArrayList<>();
+        FirebaseFirestore.getInstance().collection("atletas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(DocumentSnapshot doc : task.getResult().getDocuments()){
+                                atletas.add((String) doc.get("nome"));
+                            }
+                            setAtletas(atletas);
+                        }else{
+                            Log.e(DEBUG_TAG, "Erro: " + task.getException().toString());
+                            Toast.makeText(Funcoes.this, "Verifique sua conexão. Não conseguimos conectar ao banco de dados...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void setAtletas(List<String> atletas){
+        mAtletas = atletas;
+        Log.d(DEBUG_TAG, Arrays.toString(mAtletas.toArray()));
+    }
+
+
 }
--- app/src/main/java/com/techdev/nwsconnect/MainActivity.java
+++ app/src/main/java/com/techdev/nwsconnect/MainActivity.java
@@ -1,10 +1,24 @@
 package com.techdev.nwsconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends Funcoes {
 
@@ -13,9 +27,8 @@ public class MainActivity extends Funcoes {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_after_login);
 
-
-
-
+        //Ao criar o app, buscar as informações de atletas para ter em um string array
+        buscarAtletas();
     }
 
 
